347deb5a965cd886101eb72119174981f2328042
Kameron Sheppard <kameron.x.sheppard@gmail.com>
2022-03-13 16:48:48 -0400

    Enhance script with progress messages
    Add progress message so it's clear how long the script will take

--- leet_code_problems.py
+++ leet_code_problems.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+# from the following gist: https://gist.github.com/akshaykarnawat/6479579a351f6fe52aab4042829ca967
 import requests
 import json
-import time
 import os
 
-LOC_TO_STORE = '/Volumes/ExPac/dev/python/leet_code_problems'
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+NC = "\033[0m"
+
+LOC_TO_STORE = os.getcwd()
 
 LEET_CODE_URL = 'https://leetcode.com'
 LEET_CODE_GRAPHQL_URL = 'https://leetcode.com/graphql' 
@@ -36,14 +41,18 @@ HEX_LIKE_URL_2 = '''%22}&query=query%20questionData($titleSlug:%20String!)%20{%2
 
 
 # get to the problem one by one and start downloading based on difficulty
-# 1 = EASY; 2 = MEDIUM; 3 = HARD
+difficulty_as_string_map = {1: "easy", 2: "medium", 3: "hard"}
 for difficulty in problems:
-    
-    # create a new folder in local path
-    if not os.path.exists('/'.join([LOC_TO_STORE, str(difficulty)])):
-        os.makedirs('/'.join([LOC_TO_STORE, str(difficulty)]))
+    difficulty_as_string = difficulty_as_string_map[difficulty]
 
-    for title, pUrl in problems.get(difficulty).items():
+    print("Beginning difficulty: %s%s%s" % (GREEN, difficulty_as_string, NC))
+    # create a new folder in local path
+    if not os.path.exists('/'.join([LOC_TO_STORE, str(difficulty_as_string)])):
+        os.makedirs('/'.join([LOC_TO_STORE, str(difficulty_as_string)]))
+
+    problemItems = problems.get(difficulty).items()
+    for idx, (title, pUrl) in enumerate(problemItems):
+        print("Problem %d of %d for %s" % (idx + 1, len(problemItems), difficulty_as_string))
         res = requests.get(HEX_LIKE_URL_1 + title + HEX_LIKE_URL_2)
         if res.status_code != 200:
             continue
@@ -53,16 +62,13 @@ for difficulty in problems:
         quesData = d.get('data', {}).get('question', {})
         titleSlug = quesData.get('titleSlug', '')
         content = quesData.get('content', '')
+        if content is None:
+            continue
 
         # save data to local folder
-        with open('{0}/{1}/{2}.html'.format(LOC_TO_STORE, str(difficulty), titleSlug), 'w+') as f:
+        with open('{0}/{1}/{2}.html'.format(LOC_TO_STORE, difficulty_as_string, titleSlug), 'w+') as f:
             try:
                 f.writelines(content)
                 f.close()
             except Exception as e:
-                print(title, 'had a problem while saving!', e.args[0])
-
-        # maybe wait a little so that server does seem like there is something fishy with all the pulls
-        # although I doubt this even matters since 30 sec is not enough of a wait; they will know regardless
-        # since we are pulling everything all
-        # time.sleep(30)
+                print('%s%s had a problem while saving!%s' % (RED, title, e.args[0], NC))
